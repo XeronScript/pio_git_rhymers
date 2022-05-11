98277ab7a44dbac67e21013992a4e29a725d52f2
Kamil Sasin 235962 <235962@edu.p.lodz.pl>
2022-05-11 13:33:21 +0200

    1.4 accessory i hermetyzacja

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -3,34 +3,40 @@ package edu.kis.vh.nursery;
 public class DefaultCountingOutRhymer {
 
     public static final int MINUS_ONE = -1;
-    public static final int TWELVE = 12;
-    private final int[] NUMBERS = new int[TWELVE];
+    public static final int MAX_SIZE = 12;
+    private final int[] NUMBERS = new int[MAX_SIZE];
 
-    public int total = MINUS_ONE;
+    private int total = MINUS_ONE;
 
     public void countIn(int in) {
-        if (!isFull())
-            NUMBERS[++total] = in;
+        if (!isFull()) {
+            total++;
+            NUMBERS[getTotal()] = in;
+        }
     }
 
     public boolean callCheck() {
-        return total == MINUS_ONE;
+        return getTotal() == MINUS_ONE;
     }
 
     public boolean isFull() {
-        return total == TWELVE - 1;
+        return getTotal() == MAX_SIZE - 1;
     }
 
     protected int peekaboo() {
         if (callCheck())
             return MINUS_ONE;
-        return NUMBERS[total];
+        return NUMBERS[getTotal()];
     }
 
     public int countOut() {
         if (callCheck())
             return MINUS_ONE;
-        return NUMBERS[total--];
+        total--;
+        return NUMBERS[getTotal()];
     }
 
+    public int getTotal() {
+        return total;
+    }
 }
